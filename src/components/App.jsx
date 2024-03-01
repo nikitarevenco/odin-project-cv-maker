c4193683a29ce20f0f144e75604225d69e5fa3f8
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-03-01 16:55:23 +0000

    feat: make cv header editable

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,23 @@ import { useState } from "react";
 import "../styles/App.css";
 import CV from "./CV";
 import { initialInfo, initialSections } from "../data/initial-info";
+import EditCVBasics from "./EditCVBasics";
 
 function App() {
   const [info, setInfo] = useState(initialInfo);
   const [sections, setSections] = useState(initialSections);
-  return <CV info={info} sections={sections} />;
+
+  const CVSectionEdit = Object.entries(sections).map((section) => {
+    <EditCVSection section={section} />;
+  });
+
+  return (
+    <>
+      <EditCVBasics info={info} setInfo={setInfo} />
+      {CVSectionEdit}
+      <CV info={info} sections={sections} />
+    </>
+  );
 }
 
 export default App;
